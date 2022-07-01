f8e284bfaf2d9a1c5b836bc862c2dce86e3ebfc7
alen.infantino <alen.infantino@gmail.com>
2022-07-01 19:01:46 -0300

    ...

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+Vue.createApp({
+    data() {
+        return {
+            pagina: "home",
+
+        }
+    },
+
+    created() {
+
+    },
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+
+    computed: {
+
+    }
+}).mount('#app')
